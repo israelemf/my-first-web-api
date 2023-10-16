54bb223f10d423532a3156f665d81fe1a2f59d68
Israel Meloso Filho <israelemf@outlook.com>
2023-10-15 22:20:35 -0300

    Adicionado PostMapping do User + RequestMapping

--- src/main/java/curse/myfirstwebapi/controller/UserController.java
+++ src/main/java/curse/myfirstwebapi/controller/UserController.java
@@ -8,27 +8,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     // Um Controller é apenas uma chamada a camada de serviço
     @Autowired
     private UserRepository userRepository;
 
     // URI para buscar os usuarios do banco
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
     //Buscar o parâmetro do método através da informação passada na URI
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public User getUser(@PathVariable("username") String username) {
         return userRepository.findByUsername(username);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Integer id) {
         userRepository.deleteById(id);
 
         return "Usuário " + id + " excluido";
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        userRepository.save(user);
+
+        return user;
     }
 }
