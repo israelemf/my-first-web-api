427738bced4f28afc3b7a77e4703aebd5fc37cd0
Israel Meloso Filho <israelemf@outlook.com>
2023-10-15 22:36:57 -0300

    Swagger | Implementado método Contact

--- src/main/java/curse/myfirstwebapi/doc/SwaggerConfig.java
+++ src/main/java/curse/myfirstwebapi/doc/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package curse.myfirstwebapi.doc;
 
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private Contact contact() {
+        return new Contact("Israel", "site", "israelemf@outlook.com");
+    }
 
 }
